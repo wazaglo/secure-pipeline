4f375cef241b3b0598b4df40176535820a3a9d95
wazaglo <wazaglo87@gmail.com>
2026-07-06 16:43:06 +0000

    Fix AMI filter for Ubuntu 22.04

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
+    values = ["ubuntu-jammy-22.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
